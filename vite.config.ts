82bcd893e6c627b5fb1873c7396214eff79206f4
HDTK7897\dora7 <hdtk7897@gmail.com>
2025-08-02 14:59:12 +0900

    github pages公開設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],  
+  base: '/aquarium_dashboard/', // GitHub Pagesのリポジトリ名を指定
 })
